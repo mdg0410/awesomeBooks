49b3fb6b2bdb50e3f760ed91e88adb84b6036257
mdg0410 <levoyersteven@gmail.com>
2023-02-21 15:11:36 -0500

    Add logic at the project

--- js/main.js
+++ js/main.js
@@ -1,3 +1,54 @@
-const hello = document.querySelector('.title');
+let books = [
 
-hello.innerHTML += ' !!';
+];
+
+const booksContainer = document.getElementById('containerBooks');
+
+function saveData() {
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+function removeBook(index) {
+  books.splice(index, 1);
+  saveData();
+}
+
+const render = () => {
+  booksContainer.innerHTML = '';
+  for (let i = 0; i < books.length; i += 1) {
+    const item = document.createElement('li');
+    item.innerHTML = `<span class="title">${books[i].title}</span> <br> ${books[i].author} <br>`;
+    const button = document.createElement('button');
+    const divider = document.createElement('hr');
+    button.textContent = 'Remove';
+    item.append(button);
+    item.append(divider);
+    button.onclick = () => {
+      removeBook(i);
+      render();
+    };
+    booksContainer.append(item);
+  }
+};
+
+window.onload = () => {
+  if (localStorage.getItem('books')) {
+    books = JSON.parse(localStorage.getItem('books'));
+  }
+
+  render();
+};
+
+function addBook(title, author) {
+  books.push({ title, author });
+  render();
+}
+
+document.forms[0].onsubmit = (e) => {
+  e.preventDefault();
+  const thisForm = e.target;
+  const title = thisForm[0].value;
+  const author = thisForm[1].value;
+  addBook(title, author);
+  saveData();
+};
